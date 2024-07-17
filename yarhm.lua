bba38a596ac5064f937cb3ef96a563ce024d0129
JoystickpIays <bryllecorro03@gmail.com>
2024-07-17 11:58:06 +0800

    yarhm.lua

--- yarhm.lua
+++ yarhm.lua
@@ -1,3 +1,3 @@
 -- loadstring(game:HttpGet("https://pastebin.com/raw/baYkHU3M"))()
 
-loadstring(game:HttpGet("https://rentry.co/yarhm-goteamst-scr"))()
+loadstring(game:HttpGet("https://rentry.co/yarhm-goteamst-scr/raw"))()
