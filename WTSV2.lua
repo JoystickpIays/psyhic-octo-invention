3f87b575f8d2a1dffa6f19b6d8d00f0d6dd1debb
JoystickpIays <bryllecorro03@gmail.com>
2024-08-23 17:17:31 +0800

    WTSV2.lua

--- WTSV2.lua
+++ WTSV2.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/psychic-octo-invention/main/MTSV2"))()
 
 getgenv().gui = false
-loadstring(game:HttpGet("https://egorikusa.space/0e6b45d4f77d8305eb3be7db.lua", true))()
+loadstring(game:HttpGet("https://egorikusa.space/937d969cfb789b4fbf1400da.lua", true))()
