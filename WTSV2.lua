d13ccc9fafde1539904cb223f6616f26a357a217
JoystickpIays <bryllecorro03@gmail.com>
2024-10-19 12:45:40 +0800

    Update WTSV2.lua

--- WTSV2.lua
+++ WTSV2.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/psychic-octo-invention/refs/heads/main/Dupe.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/psychic-octo-invention/refs/heads/main/notification.lua"))()
 
