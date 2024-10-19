58537a92929119d10f5859fc1bd2ac6287ec3538
JoystickpIays <bryllecorro03@gmail.com>
2024-10-19 12:33:28 +0800

    Update WTSV2.lua

--- WTSV2.lua
+++ WTSV2.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/nexus/main/Dupe"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/psychic-octo-invention/refs/heads/main/ChroniclesVIP.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/psychic-octo-invention/refs/heads/main/Dupe.lua"))()
 
