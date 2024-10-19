a10b8cd83cdc29700a811a96c4b66663448bc9a7
JoystickpIays <bryllecorro03@gmail.com>
2024-10-19 12:29:32 +0800

    Update ChroniclesVIP.lua

--- ChroniclesVIP.lua
+++ ChroniclesVIP.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/nexus/main/Dupe"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/psychic-octo-invention/refs/heads/main/notification.lua"))()
 
 if false then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/nexus/main/Dupe"))()
