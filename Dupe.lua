6edb9c5463d3679dd6b439895f79fffe3f68dbdd
JoystickpIays <bryllecorro03@gmail.com>
2024-10-19 12:39:28 +0800

    Update Dupe.lua

--- Dupe.lua
+++ Dupe.lua
@@ -1,6 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/psychic-octo-invention/refs/heads/main/notification.lua"))()
-
-if false then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/nexus/main/Dupe"))()
-end
-
